5715139b551bdaf409f4116d4598139288761e5b
Ovy-Evbodi <evbodiovo@gmail.com>
2022-11-06 00:16:37 +0100

    Add getenv feature

--- main.h
+++ main.h
@@ -5,9 +5,18 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t _getline(char **line_buff, ssize_t *capacity, size_t stream);
 int _strcmp(char *s1, char *s2);
+char *_strtok(char *str, const char *delim);
+size_t _strlen(char *str);
+char *_strchr(char *s, char c);
+char *_strcat(char *dest, char *src);
+char **_strtow(char *str);
 
 #endif /*MAIN_H*/
