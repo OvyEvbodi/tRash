0e2313ebb069bfeaa06cee832a3ec5e95f983548
chee-zaram <47122910+chee-zaram@users.noreply.github.com>
2022-11-08 18:38:31 +0100

    Add file with function that run execve

---
+++ execve.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * exec_cmd - Handles execution of a commands.
+ * @buffer: Commandline buffer used by _getline.
+ * @arr_tokens: Array of tokens.
+ * @cmd_full_path - Full path to command.
+ * @env: List of environment variables.
+ */
+void exec_cmd(char *buffer, char **arr_tokens, char *cmd_full_path, char **env)
+{
+	char *echo_arg_string = NULL;
+	pid_t pid = fork();
+	arr_tokens[0] = cmd_full_path;
+
+	if (strstr(cmd_full_path, "echo") == 0)
+		echo_arg_string = sort_echo(&arr_tokens, env);
+
+	if (pid == 0)
+	{
+		if (execve(cmd_full_path, arr_tokens, env) == -1)
+		{
+			write(2, "Error: exec failed\n", 19);
+			free(cmd_full_path);
+			if (echo_arg_string)
+				free(echo_arg_string);
+			free(arr_tokens);
+			free(buffer);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (echo_arg_string)
+			free(echo_arg_string);
+		free(cmd_full_path);
+		free(arr_tokens);
+		free(buffer);
+	}
+}
+
