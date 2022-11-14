dc68ee283b0a4eaa765002b4f6f13176e867c6fe
Ovy-Evbodi <evbodiovo@gmail.com>
2022-11-14 21:30:52 +0100

    Merge string functions

--- string_funcs.c
+++ string_funcs.c
@@ -1,6 +1,42 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
+
+/**
+ * _strlen - counts the characters in a string
+ * @str: the string to count
+ *
+ * Return: the number of characters in a string
+ */
+
+size_t _strlen(char *str)
+{
+	if (!*str)
+		return (0);
+	return (1 + _strlen(str + 1));
+}
+
+/**
+ * _strncmp - Compares two strings up to n bytes.
+ * @s1: First argument, and string to the be compared to.
+ * @s2: Second argument, and string to be compared.
+ * @n: Number of bytes to compare.
+ *
+ * Return: Returns a - number if s1 less than s2, 0 if s1 equal to s2,
+ * or a + number if s1 greater than s2.
+ */
+
+int _strncmp(char *s1, char *s2, size_t n)
+{
+	size_t i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == 0 || i == n - 1)
+			break;
+		i++;
+	}
+
+	return (s1[i] - s2[i]);
+}
 
 /**
  * count_word - helper function to count the number of words in a string
@@ -36,6 +72,7 @@ int count_word(char *s)
  *
  * Return: a pointer to the array of words
  */
+
 char **_strtow(char *str)
 {
 	char **matrix, *tmp;
@@ -77,3 +114,24 @@ char **_strtow(char *str)
 
 	return (matrix);
 }
+
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: the string to scan
+ * @c: the character to search for
+ *
+ * Return: a pointer to the first occurence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
+
