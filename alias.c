54f118c77825643a6fcc13a0f2c00cc0df066fc7
Ovy-Evbodi <evbodiovo@gmail.com>
2022-11-15 04:47:08 +0100

    Add function to create/add alias

--- alias.c
+++ alias.c
@@ -48,14 +48,62 @@ void split_alias(char *alias_str, char **alias, char **expanded, int *shrt_len, 
 
 }
 
+/**
+ * add_alias - adds an alias definition to the alias list,
+ * or overwrites an existing definition
+ * @head: a pointer to the head pointer
+ * @alias: the alias
+ * @expanded: th command to expand alias into
+ *
+ * Return: a pointer to the new alias,
+ * otherwise, NULL
+*/
+
+
+alias_t *add_alias(alias_t **head, char **alias, char **expanded)
+{
+	alias_t *new;
+	alias_t *temp = *head;
+
+	new = malloc(sizeof(alias_t));
+	if (!new)
+		return (NULL);
+	new->cmd = "alias";
+	new->shrt = _strdup(*alias);
+	new->full = _strdup(*expanded);
+	new->equal = '=';
+	new->pre = '\'';
+	new->post = '\'';
+	new->link = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (temp->link)
+		temp = temp->link;
+	temp->link = new;
+
+	// free(alias);
+	// free(expanded);
+
+	return (new);
+}
+
 
 int main(void)
 {
 	char *alias = NULL, *expanded = NULL;
 	int shrt_len = 0, full_len = 0;
+	alias_t *head = NULL, *node;
 
 	printf("%s %s\n", alias, expanded);
-	split_alias("push=smore and", &alias, &expanded, &shrt_len, &full_len);
+	split_alias("push='smore and'", &alias, &expanded, &shrt_len, &full_len);
 	printf("\n%s %s\n", alias, expanded);
 	printf("%d %d\n", shrt_len, full_len);
+	node = add_alias(&head, &alias, &expanded);
+	printf("%s, %s, %s, %c, %c \n", node->cmd, node->shrt, node->full, node->pre, node->equal);
+
 }
--- alias.h
+++ alias.h
@@ -3,7 +3,7 @@
 
 /**
  * struct alias - defines an alias
- * @alias: the alias command
+ * @cmd: the alias command
  * @shrt: the short form
  * @full: the full command to expand shrt into
  * @pre: the prefix for printing the expanded command
@@ -12,16 +12,18 @@
 
 
 typedef struct aliases {
-	char *alias;
+	char *cmd;
 	char *shrt;
 	char *full;
+	struct aliases *link;
 	char equal;
 	char pre;
 	char post;
-} alias;
+} alias_t;
 
 /* prototypes */
 void split_alias(char *alias_str, char **alias, char **expanded, int *shrt_len, int *full_len);
+alias_t *add_alias(alias_t **head, char **alias, char **expanded);
 char *print_alias(char *head);
 
 
